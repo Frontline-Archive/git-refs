655cd24bf32ba1b9ca425af3cbf1067b4b416335
Michael Tuttle <openam@gmail.com>
2015-07-30 16:48:10 -0600

    test: add additional tests

---
+++ test/cwd.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require( 'should' );
+var assert = require( 'assert' );
+
+describe( 'current (cwd) directory HEAD with remotes and local branches', function () {
+	var result;
+	var error;
+
+	before( function ( done ) {
+
+		var gitRefs = require( '../' );
+
+		// By not passing in a `gitDirectory` it will use the current git folder
+		gitRefs( function ( err, refs ) {
+			result = refs;
+			error  = err;
+			done();
+		} );
+	} );
+
+	it( 'should not have thrown an error', function () {
+		assert.strictEqual( error, null );
+	} );
+
+	it( 'should have current, heads and remotes keys', function () {
+		result.should.be.of.type( 'object' );
+		result.current.should.be.of.type( 'object' );
+		result.heads.should.be.of.type( 'object' );
+		result.remotes.should.be.of.type( 'object' );
+	} );
+
+	it( 'should have correct current HEAD', function () {
+		result.current.head.should.be.of.type( 'string' ).and.be.lengthOf( 40 );
+	} );
+
+} );
---
+++ test/pull.js
@@ -0,0 +1,56 @@
+'use strict';
+
+require( 'should' );
+var assert = require( 'assert' );
+
+describe( 'pull request referenced HEAD with remotes and local branches', function () {
+	var result;
+	var error;
+
+	before( function ( done ) {
+
+		var gitRefs = require( '../' );
+
+		gitRefs( 'test/fixtures/pull', function ( err, refs ) {
+			result = refs;
+			error  = err;
+			done();
+		} );
+	} );
+
+	it( 'should not have thrown an error', function () {
+		assert.strictEqual( error, null );
+	} );
+
+	it( 'should have current, heads and remotes keys', function () {
+		result.should.be.of.type( 'object' );
+		result.current.should.be.of.type( 'object' );
+		result.heads.should.be.of.type( 'object' );
+		result.remotes.should.be.of.type( 'object' );
+	} );
+
+	it( 'should have correct current HEAD', function () {
+		result.current.head.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000002' );
+		result.current.ref.should.be.of.type( 'string' ).and.equal( 'refs/heads/pull/2' );
+	} );
+
+	it( 'should have correct local branches', function () {
+		result.heads.dev.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000011' );
+		result.heads.master.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000012' );
+	} );
+
+	it( 'should have correct remotes', function () {
+		var remotes = result.remotes;
+
+		// Should have (2) remotes (origin and upstream)
+		Object.keys( remotes ).length.should.be.of.type( 'number' ).and.equal( 2 );
+
+		// Check origin remote
+		remotes.origin.dev.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000021' );
+		remotes.origin.master.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000022' );
+
+		// Check upstream remote
+		remotes.upstream.master.should.be.of.type( 'string' ).and.equal( 'de944492d4164ceffa7600ef96c4ee6f00000032' );
+	} );
+
+} );
